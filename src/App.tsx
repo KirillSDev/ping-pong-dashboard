eca6349f4a386a32296c3b4740ee4d0f017660f2
Kirill Starodubsev <colereteps@gmail.com>
2025-01-02 16:11:14 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { Canvas } from '@react-three/fiber/native'
+
+export const App: FC = () => {
+    return <div>
+       Hello World
+    </div>
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { App } from "./App";
+import { createRoot } from "react-dom/client";
+
+const rootContainer = document.getElementById("app");
+createRoot(rootContainer).render(<App/>);
